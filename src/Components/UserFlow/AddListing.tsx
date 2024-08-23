6fe1bdf9252a6f352669a29907675d0a82d0e68e
deepthisway <contactdeep01@gmail.com>
2024-08-23 21:13:27 +0530

    added listing page and course search page

---
+++ src/Components/UserFlow/AddListing.tsx
@@ -0,0 +1,42 @@
+"use client"; 
+import * as React from "react"
+import { Button } from "@/Components/Shared/ui/button"
+import {Card, CardContent, CardFooter, CardHeader,} from "@/Components/Shared/ui/card"
+import { Input } from "@/Components/Shared/ui/input"
+import { Label } from "@/Components/Shared/ui/label"
+import Link from "next/link";
+
+function AddListing() {
+  return (
+    <main className="bg-[#56C1FF] h-screen flex items-center justify-center p-10 w-full">
+          <Card className="w-[600px] h-[550px] mx-auto p-6 shadow-lg border-2 border-blue-400">
+            <CardHeader>
+                <h2 className="text-2xl font-bold">Add your Listing</h2>
+            </CardHeader>
+            <CardContent>
+                <div className="space-y-5">
+                <div className="space-y-2">
+                    <Label htmlFor="email" className="text-lg">Title</Label>
+                    <Input type="email" id="email" placeholder="Enter your title" />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="password" className="text-lg">Description</Label>
+                    <Input type="text" id="text" placeholder="Enter Description" />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="confirmpassword" className="text-lg">Price</Label>
+                    <Input type="numbers" id="numbers" placeholder="Enter price" />
+                </div>
+                </div>
+            </CardContent>
+            <div className="flex justify-center">
+            <Button className="mt-1 w-[500px] bg-[#FDCE29] text-black hover:bg-yellow-500">Create List</Button>      
+            </div>
+            <CardFooter className="text-center mt-4">
+            
+            </CardFooter>
+            </Card>
+        </main>
+  )
+}
+ export  default AddListing;
--- src/Components/UserFlow/NavBar.tsx
+++ src/Components/UserFlow/NavBar.tsx
@@ -26,6 +26,8 @@ const Navbar = () => {
             </Link>
             </div>
             <div>
+              <Link href="/Userflow/addListing" className="bg-blue-300 text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-blue-200"> Add Listing
+              </Link>
             <Link href="/join" className="text-gray-700 hover:text-black px-3 py-2 rounded-md text-sm font-medium">
               Join as Teacher
             </Link>
@@ -74,6 +76,7 @@ const Navbar = () => {
           <Link href="/contact" className="text-gray-700 hover:text-black block px-3 py-2 rounded-md text-base font-medium">
             Contact
           </Link>
+          <Link href=''> Add Listing</Link>
           <Link href="/join" className="text-gray-700 hover:text-black block px-3 py-2 rounded-md text-base font-medium">
             Join as Teacher
           </Link>
