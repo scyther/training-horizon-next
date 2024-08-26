a37d5a6765a324f0dcebb1ecc0ff32e97d1caa19
Gaurang_02 <heygaurang.02@gmail.com>
2024-08-26 20:41:30 +0530

    description page

---
+++ src/app/courses/category/description/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    
+    <div className="flex justify-center items-center h-screen w-full">
+        <h1 className="flex justify-center">descripton page</h1>
+    </div>
+  );
+}
