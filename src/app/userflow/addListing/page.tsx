ca071051fbc565eb06c5630cf989588fb6e5c42d
Sakchamn <143090295+Sakchamn@users.noreply.github.com>
2024-08-28 19:23:44 +0530

    add listing form

--- src/app/userflow/addListing/page.tsx
+++ src/app/userflow/addListing/page.tsx
@@ -1,9 +1,12 @@
-import AddListing from '@/components/UserFlow/AddListing'
+
+import Navbar from '@/components/trainer-dashboard/NavBar'
+import { AddListing } from '@/components/UserFlow/AddListing'
 import React from 'react'
 
 const listingPage = () => {
   return (
     <>
+    <Navbar/>
     <AddListing/>
     </>
   )
