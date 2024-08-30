56c726d4c634ae8683e5e45bd9a91d982d351b96
azadDsync <domendrasinghkomra750@gmail.com>
2024-08-30 21:45:18 +0530

    added backend db model  for user,listing and trainer

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 
-// mongoose.connect("mongodb+srv://TrainingHorizons:KT044G83XJk9neUQ@cluster0.0s68i.mongodb.net/Test")
+// please put the mongoDb connnection URL
 
 module.exports = mongoose;
