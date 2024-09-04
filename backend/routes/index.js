c3f6329bb7780bf646aa6f6524c40c01883b74d9
deepthisway <contactdeep01@gmail.com>
2024-09-04 15:55:04 +0530

    Minor changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,11 @@
 const express =require("express");
 const userRouter=require("./user")
 const listingRouter=require("./listing")
-const trainerRouter =require("./trainer")
 const adminRouter = require("./adminRoutes")
 
 const router=express.Router();
-// const app=express();
-// app.use("/api/v1/user",userRouter);
 
 router.use("/user",userRouter);
-router.use("/trainer",trainerRouter);
 router.use("/listing",listingRouter);
 router.use("/admin",adminRouter);
 
